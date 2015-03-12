930eec840ff3a9836bbc628cc0dcd733dafc5c88
Damir <damir@withpulp.com>
2015-03-11 23:26:30 -0400

    added leaderboard route and set path in footer menu

--- routes/routes.js
+++ routes/routes.js
@@ -4,3 +4,17 @@ Router.route('/', {
   this.render('home');
   SEO.set({ title: Meteor.App.NAME });
 });
+
+Router.route('/leaderboard', {
+  name: 'leaderboard'
+}, function () {
+  this.render('leaderboard');
+  SEO.set({ title: Meteor.App.NAME });
+});
+
+Router.route('/dashboard', {
+  name: 'dashboard'
+}, function () {
+  this.render('dashboard');
+  SEO.set({ title: Meteor.App.NAME });
+});
