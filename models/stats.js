5e4721d46b6b212ea30f404d2482b26acfb84bca
Damir <damir@withpulp.com>
2015-03-15 22:15:25 -0400

    added Stats allow method to allow insert for tracking event

--- models/stats.js
+++ models/stats.js
@@ -1,1 +1,13 @@
 Stats = new Mongo.Collection('stats');
+
+Stats.allow({
+	'insert': function(userId, doc) {
+		return !!userId;
+	},
+	'update': function(userId, doc) {
+		return !!userId;
+	},
+	'remove': function(userId, doc) {
+		return !!userId;
+	}
+});
