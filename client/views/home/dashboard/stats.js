5b55b53e03a42207f3a9cb3cc60401615241ba44
Damir <damir@withpulp.com>
2015-03-21 02:39:47 -0400

    stats currentStreak helper wip

--- client/views/home/dashboard/stats.js
+++ client/views/home/dashboard/stats.js
@@ -1,6 +1,6 @@
 Template['stats'].helpers({
   currentStreak: function() {
-    return Acts.find({}, {days: 1});
+    return Acts.find({ field: { days: 1} });
   },
   today: function() {
     return moment().format('MMMM Do');
